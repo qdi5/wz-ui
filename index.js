eef84aa5952ab795ae50e6c8639cbc2d765c399c
qdi5 <969296206@qq.com>
2020-05-08 00:46:35 +0800

    发布代码到npm仓库

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2020-05-08 00:10:36
+ * @LastEditors: wuzhe
+ * @LastEditTime: 2020-05-08 00:31:44
+ * @FilePath: c:\workspace\repository-everyday\wz-ui\index.js
+ * @Description: 
+ */
+import "font-awesome/css/font-awesome.min.css";
+import './dist/wz-ui.min.css'
